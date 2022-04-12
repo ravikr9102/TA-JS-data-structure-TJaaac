b5706b4f98b80dbbcbcffc1edb98cca03645044d
Ravi kumar <rkravikr9102@gmail.com>
2022-04-12 23:03:42 +0530

    refactor

--- block-BJaaci/code/index.js
+++ block-BJaaci/code/index.js
@@ -36,7 +36,7 @@ movies[0].title;
 
 // 2. Log the value of the title key in the last movie of the array named `movies`
 
-movies[2].title;
+movies[movies.length - 1 ].title;
 
 // 3. Log the value of the title key in the second movie (index 1) of the array named `movies`
 
@@ -56,7 +56,7 @@ movies[0].actors[0];
 
 // 7. Log the name of the last actor of the first movie
 
-movies[0].actors[2];
+movies[0].actors[movies[0].actors.length - 1];
 
 // 8. Log the name of the second actor (index 1) of the first movie
 
@@ -68,7 +68,7 @@ movies[1].actors[1];
 
 // 10. Log the name of the last actor of the thrid movie
 
-movies[2].actors[2];
+movies[2].actors[movies[0].actors.length - 1];
 
 // 11. Log the name of the second actor (index 1) of the third movie
 
@@ -88,37 +88,30 @@ movies[0].genres[0];
 
 // 15. Log the first genres of the second movie
 
-movies[1].genres[1];
+movies[1].genres[0];
 
 // 16. Log the last genres of the first movie (using the length property of array)
 
 // let gen=movies[0].genres.length-1
 
-movies[0].genres.length - 1;
+movies[0].genres[movies[0].genres.length-1];
 
 // 17. Log the first genres of the second movie (using the length property of array)
 
+movies[1].genres[0];
 
 // 18. Log all the genres of the first movie one by one
 
-// console.log(movies[0].genres);
-// console.log(movies[i]);
 
-// for(i = 0; i < movies[i].genres.length; i++) {
-//   console.log(genres[i]);
-// }
-// for(i = 0; i < movies[0].genres.length; i++) {
-//   console.log(movies[i]);
-// }
-
+for(let genre of movies[0].genres);
 
 // 19. Log all the genres of the second movie one by one
 
+for(let genre of movies[1].genres);
 
 // 20. Log if the first actor of the first movie is `Rachel McAdams` or not (You have to log true or false)
 
 movies[0].actors[0] == "Rachel McAdams";
-
 
 
 // 21. Log if the second actor (index 1) of the second movie is `Natalie Portman` or not (You have to log true or false)
@@ -128,14 +121,14 @@ movies[1].actors[1] == "Natalie Portman";
 
 // 22. Log if the year of all three movies is greater than `2017` or not one by one. (Log true or false)
 
-
+for(let movie of movies) {
+  console.log(movies.year > 2017);
+}
 
 // 23. Log the title of all three movies one by one.
 
-for(i = 0; i < movies.length; i ++) {
-  for( j = 0; j < movies[i].title.length; j++) {
-    console.log(movies[i].title[j]);
-  }
+for(let movie of movies) {
+  console.log(movie.title)
 }
 
 // 24. Log if the title of the first movie is `Hannah` or not
@@ -144,24 +137,46 @@ movies[0].title == "Hannah"
 
 // 25. Log the number of actors in all three movies one by one
 
-// for(i = 0; i < movies.length; i++) {
-//   for (j = 0; j < movies[i].actors.length; j++) {
-//     console.log(movies[i].actors[j]);
-//   }
-// }
+for(i = 0; i < movies.length; i++) {
+  for (j = 0; j < movies[i].actors.length; j++) {
+    console.log(movies[i].actors[j]);
+  }
+}
 
 // 26. Log the number of genres in all three movies one by one
 
-// for(i = 0; i < movies.length; i ++) {
-//   for( j = 0; j < movies[i].genres.length; j++) {
-//     console.log(movies[i].genres[j]);
-//   }
-// }
+for(i = 0; i < movies.length; i ++) {
+  for( j = 0; j < movies[i].genres.length; j++) {
+    console.log(movies[i].genres[j]);
+  }
+}
 
 // 27. Log the name of all the movies with more than 1 genre
 
+for(let movie of movies) {
+  if (movie.genres.length > 1) {
+    console.log(movie.title,"name");
+  }
+}
 // 28. Log the name of all the movies with more than 1 actors
 
+for(let movie of movies) {
+  if (movie.actors.length > 1) {
+    console.log(movie.title,"name");
+  }
+}
 // 29. Log the name of all the movies with exactly 3 actors
 
+for(let movie of movies) {
+  if (movie.genres.length === 3) {
+    console.log(movie.title,"name");
+  }
+}
+
 // 30. Log the name of all the movies with year `2018`
+
+for(let movie of movies) {
+  if(movie.year === 2018) {
+    console.log(movie.title, "year");
+  }
+}
